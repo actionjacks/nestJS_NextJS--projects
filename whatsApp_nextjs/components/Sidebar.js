f39d1f8af247f17fc05413da8ae6ca6a71e62081
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-07-31 23:11:23 +0200

    whatsapp first commit

---
+++ whatsApp_nextjs/components/Sidebar.js
@@ -0,0 +1,90 @@
+import { Avatar, IconButton, Button } from "@material-ui/core";
+import styled from "styled-components";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+import * as EmailValidator from "email-validator";
+
+function Sidebar() {
+  const createChat = () => {
+    const input = prompt(
+      "Enter an email address for the user u wish to chat witch"
+    );
+    if (!input) return;
+
+    if (EmailValidator.validate(input)) {
+    }
+  };
+  return (
+    <Container>
+      <Header>
+        <UserAvatar />
+
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
+
+      <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
+    </Container>
+  );
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+
+const SidebarButton = styled(Button)`
+  width: 100%;
+  /* important */
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+  transition: 0.3s;
+  :hover {
+    opacity: 0.6;
+  }
+`;
+
+const IconsContainer = styled.div``;
---
+++ whatsApp_nextjs/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBa-e13x7ZFaYrxKZU-qRD2LFe8UnV2ejU",
+  authDomain: "jackmessenger-e9aff.firebaseapp.com",
+  databaseURL: "https://jackmessenger-e9aff.firebaseio.com",
+  projectId: "jackmessenger-e9aff",
+  storageBucket: "jackmessenger-e9aff.appspot.com",
+  messagingSenderId: "308423447401",
+  appId: "1:308423447401:web:22f0b3716d11f1da79a3f8",
+  measurementId: "G-7H5RBBC0R6",
+};
+
+const app = !firebase.app.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+const auth = app.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
