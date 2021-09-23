40e79a40a66127d393f90bf90e5dc2af1dc90b30
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-09-23 13:29:49 +0200

    first styles form header

--- next-js/amazon-clone/components/Header.js
+++ next-js/amazon-clone/components/Header.js
@@ -1,8 +1,31 @@
+import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
 function Header() {
   return (
-    <div>
-      <p></p>
-    </div>
+    <header>
+      <div className="flex items-center p-1 bg-black flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            src="/logo.png"
+            width={70}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400">
+          <input type="text" placeholder="search" />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+      </div>
+      {/*  */}
+      <div></div>
+    </header>
   );
 }
 
--- next-js/amazon-clone/tailwind.config.js
+++ next-js/amazon-clone/tailwind.config.js
@@ -1,19 +1,9 @@
 module.exports = {
   mode: "jit",
-  purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        amazon_blue: {
-          light: "#232F3E",
-          DEFAULT: "#131921",
-        },
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
