36f29a3048e0b6b0292838cbed83de413286f6b7
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-10-06 22:27:03 +0200

    add header banner

---
+++ next-js/airbnbclone/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Fake AirBNB</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <Banner />
+    </div>
+  );
+}
