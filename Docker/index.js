b8fcbbb79e59c0ed52902017a2b9360843eea78d
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-11-27 17:25:23 +0100

    docker

---
+++ Docker/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send([
+    {
+      name: "jacek",
+      email: "jacek-dupa@o2.pl",
+    },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
