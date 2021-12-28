9323b21e8a8d05c007036659577c45ae0806d20d
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-12-28 21:45:12 +0100

    d

---
+++ Vue/vue-cli/vuex-eshoop/src/store/index.js
@@ -0,0 +1,41 @@
+import { createStore } from 'vuex';
+
+import productsModule from './modules/products';
+import cartModule from './modules/cart';
+
+const store = createStore({
+  modules: {
+    prods: productsModule,
+    cart: cartModule,
+  },
+  state() {
+    return {
+      isLoggedIn: true,
+    };
+  },
+  mutations: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+  },
+
+  actions: {
+    login(context) {
+      context.commit('login');
+    },
+    logout(context) {
+      context.commit('logout');
+    },
+  },
+
+  getters: {
+    isAuthenticated(state) {
+      return state.isLoggedIn;
+    },
+  },
+});
+
+export default store;
