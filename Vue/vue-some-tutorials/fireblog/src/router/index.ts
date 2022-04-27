16fea1c45fb1ecaeb73deacabddfde316a890b9e
Jack <actionjacks22@gmail.com>
2022-04-27 23:18:57 +0200

    s

--- Vue/vue-some-tutorials/fireblog/src/router/index.ts
+++ Vue/vue-some-tutorials/fireblog/src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import Blogs from '@/views/Blogs.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/blogs',
+    name: 'Blogs',
+    component: Blogs
   }
 ]
 
