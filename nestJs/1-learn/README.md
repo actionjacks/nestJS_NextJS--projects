3c953e14d93520141db7fc736a22ec743201a7f2
jaxoo <actionjacks22@gmail.com>
2022-07-10 23:14:31 +0200

    2

--- nestJs/1-learn/README.md
+++ nestJs/1-learn/README.md
@@ -2,3 +2,15 @@ start dev-db (service name is form docker-compose.yml)
 ```
 docker-compose up -d
 ```
+
+.env file - from setting docker-compose.yml
+```
+DATABASE_URL="postgresql://postgres:123@localhost:5434/nest?schema=public"
+```
+
+run if buil new skima model in schema.prisma
+```
+npx prisma migrate dev
+
+npx prisma studio
+```
--- nestJs/1-learn/src/auth/auth.controller.ts
+++ nestJs/1-learn/src/auth/auth.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
-  signup() {
+  signup(
+    dto: AuthDto,
+    // @Body('email') email: string,
+    // @Body('password') password: string,
+  ) {
     this.authService.signup();
   }
 
