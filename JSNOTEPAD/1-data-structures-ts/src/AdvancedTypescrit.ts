713afc02b67af025fa4b64e68a271c8323a09375
j-zablocki <actionjacks22@gmai.com>
2022-07-29 23:30:48 +0200

    1

--- JSNOTEPAD/1-data-structures-ts/src/AdvancedTypescrit.ts
+++ JSNOTEPAD/1-data-structures-ts/src/AdvancedTypescrit.ts
@@ -159,3 +159,100 @@ const busEngine: BusEngine = {
 const carEngine: Engine<Car> = {
   type: "hybrid",
 };
+
+const backlog = {
+  releases: [
+    {
+      name: "lotem",
+      epics: [
+        {
+          name: "lore ",
+        },
+      ],
+    },
+  ],
+};
+type Unarray<T> = T extends Array<infer U> ? U : T;
+type Release = Unarray<typeof backlog["releases"]>;
+
+/*
+  partials,
+  make - properties optional
+*/
+interface Starship {
+  name: string;
+  enablehiperdrive: boolean;
+}
+
+const updateStarship = (starship: Partial<Starship>) => {
+  console.log(starship);
+};
+updateStarship({ name: "jacel" });
+
+/*
+  record
+*/
+const starships: Record<string, Starship> = {
+  starship1: {
+    name: "jp",
+    enablehiperdrive: true,
+  },
+};
+
+/*
+  pick - pick values
+   - omit - dont take spec value
+*/
+const nameOfStarship: Pick<Starship, "name"> = {
+  name: "klll",
+};
+const nameOfStarship2: Omit<Starship, "name"> = {
+  enablehiperdrive: false,
+};
+
+/*
+  return types
+*/
+function paintStarship(id: number, color: string) {
+  return {
+    id,
+    color,
+  };
+}
+type paintStarshipReturnTypes = ReturnType<typeof paintStarship>;
+
+/*
+  mixin class -class get params or methods we specify and type of instance of some class
+*/
+type Contructable<ClassInstance> = new (...args: any[]) => ClassInstance;
+
+function Deletable<BaseClass extends Contructable<{}>>(Base: BaseClass) {
+  return class extends Base {
+    deleted: boolean;
+    delete() {}
+  };
+}
+
+class Class1 {
+  constructor(public name: string) {}
+}
+class Class2 {
+  constructor(public name: string) {}
+}
+
+const DeletableClass1 = Deletable(Class1);
+const DeletableClass2 = Deletable(Class2);
+// got deleted and delete method
+const useClass1 = new DeletableClass1("jacek");
+
+type DeletableClass1Instance = InstanceType<typeof DeletableClass1>;
+type DeletableClass2Instance = InstanceType<typeof DeletableClass2>;
+
+class HoldInstanceOfClass {
+  class1: DeletableClass1Instance;
+  class2: DeletableClass2Instance;
+}
+
+const main = new HoldInstanceOfClass();
+main.class1 = new DeletableClass1("jac");
+main.class2 = new DeletableClass2("jac");
---
+++ JSNOTEPAD/1-data-structures-ts/src/sortRecuretion.ts
@@ -0,0 +1,90 @@
+enum SortingDirection {
+  Asc = "asc",
+  Desc = "desc",
+}
+
+interface Class1 {
+  name: string;
+  date: Date;
+}
+
+interface Class2 {
+  label: number;
+  hightLabel: string;
+}
+
+const dataClass1: Class1[] = [
+  {
+    name: "lorem4",
+    date: new Date(2022, 03, 27),
+  },
+  {
+    name: "lorem2",
+    date: new Date(2022, 02, 27),
+  },
+  {
+    name: "lorem1",
+    date: new Date(2022, 07, 22),
+  },
+  {
+    name: "lorem10",
+    date: new Date(2022, 01, 17),
+  },
+  {
+    name: "lorem12",
+    date: new Date(2022, 01, 17),
+  },
+  {
+    name: "lorem11",
+    date: new Date(2022, 01, 17),
+  },
+];
+const dataClass2: Class2[] = [
+  {
+    label: 12,
+    hightLabel: "loremlorem",
+  },
+  {
+    label: 23,
+    hightLabel: "loremlorem2",
+  },
+  {
+    label: 920,
+    hightLabel: "loremlorem3",
+  },
+];
+
+function areClass2(dataToCheck: Class2[]): dataToCheck is Class2[] {
+  const [data] = dataToCheck;
+  return data.hightLabel !== "undefined";
+}
+
+const SORT = <T extends Class1 | Class2>(a: T, b: T) => {
+  return (label: keyof T) => {
+    return (sortDirection?: SortingDirection) => {
+      const direction = sortDirection === SortingDirection.Asc ? -1 : 1;
+      const left = a[label];
+      const right = b[label];
+
+      if (left < right) {
+        if (typeof sortDirection !== "undefined") {
+          return 1 * direction;
+        }
+        return 1;
+      }
+      if (left > right) {
+        if (typeof sortDirection !== "undefined") {
+          return -1 * direction;
+        }
+        return -1;
+      }
+      return 0;
+    };
+  };
+};
+
+const sortedClass1 = dataClass1.sort((a, b) => SORT(a, b)("date")());
+
+const sortedClass2 = areClass2(dataClass2)
+  ? dataClass2.sort((a, b) => SORT(a, b)("hightLabel")(SortingDirection.Desc))
+  : [];
