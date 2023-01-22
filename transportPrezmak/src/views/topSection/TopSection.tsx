5e4745e86e08d69350070d0d0a1661736fb3d055
dev-jack <actionjacks22@gmail.com>
2023-01-22 21:38:46 +0100

    1212

--- transportPrezmak/src/views/topSection/TopSection.tsx
+++ transportPrezmak/src/views/topSection/TopSection.tsx
@@ -14,23 +14,53 @@ function TopSection() {
       </div>
       <div className="top-section">
         <AnimeBox duration={0.4}>
-          <h1 data-aos="fade-up">Web.Auto Lorem</h1>
+          <h1 data-aos="fade-up">Prezmak -Usługi Transportowe</h1>
+          <h2 data-aos="fade-up">Polska-Anglia-Polska</h2>
         </AnimeBox>
         <AnimeBox duration={0.6}>
-          <p data-aos="fade-up">Lorem ipsum dolor</p>
+          <span
+            data-aos="slide-left"
+            className="arrow-right-icon material-symbols-outlined"
+          >
+            line_end_arrow_notch
+          </span>
+          <p data-aos="fade-up">Przewóz osób</p>
         </AnimeBox>
         <AnimeBox duration={0.8}>
-          <p data-aos="fade-up">Lorem ipsum dolor</p>
+          <span
+            data-aos="slide-left"
+            className="arrow-right-icon material-symbols-outlined"
+          >
+            line_end_arrow_notch
+          </span>
+          <p data-aos="fade-up">Transport paczek</p>
         </AnimeBox>
         <AnimeBox duration={1.2}>
-          <p data-aos="fade-up">Lorem ipsum dolor</p>
+          <span
+            data-aos="slide-left"
+            className="arrow-right-icon material-symbols-outlined"
+          >
+            line_end_arrow_notch
+          </span>
+          <p data-aos="fade-up">Przeprowadzki PL-UK-PL</p>
+        </AnimeBox>
+        <AnimeBox duration={1.4}>
+          <span
+            data-aos="slide-left"
+            className="arrow-right-icon material-symbols-outlined"
+          >
+            line_end_arrow_notch
+          </span>
+          <p data-aos="fade-up">Transport motocykli i Quadów</p>
         </AnimeBox>
 
         <AnimeBox duration={1.3}>
           <div data-aos="fade-up" className="icon-Container">
-            <span className="material-symbols-outlined">arrow_circle_right</span>
+            <span className="arrow-icon material-symbols-outlined">
+              arrow_circle_right
+            </span>
             <p data-aos="fade-up" className="link-text">
-              lorem lorem lorem
+              Napisz do nas
             </p>
           </div>
         </AnimeBox>
