7dd75f44c3604ab2bd5048a61581a2fcb6090d0b
dev-jack <actionjacks22@gmail.com>
2023-01-27 23:25:20 +0100

    1

--- transportPrezmak/src/views/topSection/TopSection.tsx
+++ transportPrezmak/src/views/topSection/TopSection.tsx
@@ -9,13 +9,9 @@ function TopSection() {
       <ContactBox />
 
       <div className="title-wrapper">
-        <AnimeBox duration={0.8}>
-          <h1 data-aos="fade">Prezmak -Usługi Transportowe</h1>
-          <h2 data-aos="fade">Przewóz osób, transport paczek, przeprowadzki</h2>
-          <AnimeBox duration={1.2}>
-            <p>Polska-Anglia-Polska</p>
-          </AnimeBox>
-        </AnimeBox>
+        <h1 data-aos="slide-left">Prezmak -Usługi Transportowe</h1>
+        <h2 data-aos="slide-left">Przewóz osób transport paczek przeprowadzki</h2>
+        <p data-aos="slide-left">Polska-Anglia-Polska</p>
       </div>
 
       <header className="header">
@@ -27,59 +23,43 @@ function TopSection() {
         </div>
       </header>
 
-      <div data-aos="fade-down">
-        <AnimeBox duration={0.8}>
-          <div className="bottom-section">
-            <div className="bottom-title">
-              <AnimeBox duration={0.5}>
-                <span
-                  data-aos="slide-left"
-                  className="arrow-right-icon material-symbols-outlined"
-                >
-                  line_end_arrow_notch
-                </span>
-                <p data-aos="fade-up">Transport paczek</p>
-              </AnimeBox>
-              <AnimeBox duration={0.7}>
-                <span
-                  data-aos="slide-left"
-                  className="arrow-right-icon material-symbols-outlined"
-                >
-                  line_end_arrow_notch
-                </span>
-                <p data-aos="fade-up">Przeprowadzki PL-UK-PL</p>
-              </AnimeBox>
-              <AnimeBox duration={0.9}>
-                <span
-                  data-aos="slide-left"
-                  className="arrow-right-icon material-symbols-outlined"
-                >
-                  line_end_arrow_notch
-                </span>
-                <p data-aos="fade-up">Transport motocykli i Quadów</p>
-              </AnimeBox>
-              <AnimeBox duration={1}>
-                <span
-                  data-aos="slide-left"
-                  className="arrow-right-icon material-symbols-outlined"
-                >
-                  line_end_arrow_notch
-                </span>
-                <p data-aos="fade-up">Przewóz osób</p>
-              </AnimeBox>
-              <AnimeBox duration={1.4}>
-                <div data-aos="fade-up" className="icon-Container">
-                  <span className="arrow-icon material-symbols-outlined">
-                    arrow_circle_right
-                  </span>
-                  <p data-aos="fade-up" className="link-text">
-                    Napisz do nas _ firma-prezmak@tlen.pl
-                  </p>
-                </div>
-              </AnimeBox>
+      <div className="bottom-section">
+        <div className="bottom-title">
+          <AnimeBox duration={0.5}>
+            <span data-aos="slide-left" className="material-symbols-outlined">
+              navigate_next
+            </span>
+            <p data-aos="fade-up">Transport paczek</p>
+          </AnimeBox>
+          <AnimeBox duration={0.7}>
+            <span data-aos="slide-left" className="material-symbols-outlined">
+              navigate_next
+            </span>
+            <p data-aos="fade-up">Przeprowadzki PL-UK-PL</p>
+          </AnimeBox>
+          <AnimeBox duration={0.9}>
+            <span data-aos="slide-left" className="material-symbols-outlined">
+              navigate_next
+            </span>
+            <p data-aos="fade-up">Transport motocykli i Quadów</p>
+          </AnimeBox>
+          <AnimeBox duration={1}>
+            <span data-aos="slide-left" className="material-symbols-outlined">
+              navigate_next
+            </span>
+            <p data-aos="fade-up">Przewóz osób</p>
+          </AnimeBox>
+          <AnimeBox duration={1.4}>
+            <div data-aos="fade-up" className="icon-Container">
+              <span className="arrow-icon material-symbols-outlined">
+                arrow_circle_right
+              </span>
+              <p data-aos="fade-up" className="link-text">
+                Napisz do nas _ firma-prezmak@tlen.pl
+              </p>
             </div>
-          </div>
-        </AnimeBox>
+          </AnimeBox>
+        </div>
       </div>
     </div>
   )
