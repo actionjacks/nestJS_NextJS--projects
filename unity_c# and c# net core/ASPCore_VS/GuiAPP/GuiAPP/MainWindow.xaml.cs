47dc2e815d37213a0a011cd6d4f770f339885c74
dev-jack <actionjacks22@gmail.com>
2023-08-05 20:01:39 +0200

    123

---
+++ unity_c# and c# net core/ASPCore_VS/GuiAPP/GuiAPP/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace GuiAPP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            myButtonName.FontSize = 18;
+            myButtonName.Content = "custom content from code";
+            myButtonName.Foreground = Brushes.Blue;
+
+            // custom text block
+            TextBlock mycustomTextBlock = new TextBlock();
+            mycustomTextBlock.FontSize = 18;
+            mycustomTextBlock.Text = "mycustom text block content";
+            // this.Content = mycustomTextBlock; // Replace window content 
+        }
+
+        private void Onclik_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+        }
+
+        private void MyButtonName_Click(object sender, RoutedEventArgs e)
+        {
+            double mySize = myLabel.FontSize;
+            myLabel.FontSize = mySize + 1;
+
+            myLabel.Content = "LABEL CHANGED FO BAR !!!";
+        }
+
+        private void MyButtonName_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+        }
+
+        private void UpdateLabel()
+        {
+            string result = "Selected options: ";
+            if (checkBox1.IsChecked == true)
+            {
+                result += "Option 1 ";
+            }
+            if (checkBox2.IsChecked == true)
+            {
+                result += "Option 2 ";
+            }
+            label.Content = result;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateLabel();
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            UpdateLabel();
+        }
+    }
+}
+// 3:18
