5a2f5dccadb760b243b9d09474c7e4d4dd537bf9
dev-jack <actionjacks22@gmail.com>
2023-09-15 19:45:22 +0200

    2

--- Astra/src/components/Nav.tsx
+++ Astra/src/components/Nav.tsx
@@ -3,6 +3,13 @@ export default function Nav({ title }: { title: string }) {
     <nav>
       {title}
       <h1>--nav--</h1>
+      <button className="border-y-4">
+        <a href="/pageone">About</a>
+      </button>
+      <br />
+      <button className="border-y-4">
+        <a href="/">Home</a>
+      </button>
     </nav>
   );
 }
