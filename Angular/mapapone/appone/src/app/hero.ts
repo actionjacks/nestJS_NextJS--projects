6da238c3bc15affc8982bbc69f3be0152ff3f57f
dev-jack <actionjacks22@gmail.com>
2023-09-23 23:01:21 +0200

    2

--- Angular/mapapone/appone/src/app/hero.ts
+++ Angular/mapapone/appone/src/app/hero.ts
@@ -2,3 +2,12 @@ export interface Hero {
   id: number;
   name: string;
 }
+
+export class Hero {
+  constructor(
+    public id: number,
+    public name: string,
+    public power: string,
+    public alterEgo?: string
+  ) {}
+}
--- Angular/mapapone/appone/src/app/hero/hero.component.ts
+++ Angular/mapapone/appone/src/app/hero/hero.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent {
+  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
 
+  model = new Hero(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
 }
