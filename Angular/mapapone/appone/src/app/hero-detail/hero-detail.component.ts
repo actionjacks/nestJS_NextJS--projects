ac6e5aed49a8761973189591c7a78ffbea1e7b1c
dev-jack <actionjacks22@gmail.com>
2023-09-23 10:28:52 +0200

    1

--- Angular/mapapone/appone/src/app/hero-detail/hero-detail.component.ts
+++ Angular/mapapone/appone/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,5 +10,26 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent {
+  constructor(
+    // The ActivatedRoute holds information about the route to this instance of the HeroDetailComponent. This component is interested in the route's parameters extracted from the URL. The "id" parameter is the id of the hero to display.
+    private route: ActivatedRoute,
+    // The HeroService gets hero data from the remote server and this component uses it to get the hero-to-display.
+    private heroService: HeroService,
+    // The location is an Angular service for interacting with the browser. This service lets you navigate back to the previous view.
+    private location: Location
+  ) {}
   @Input() hero?: Hero;
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
 }
