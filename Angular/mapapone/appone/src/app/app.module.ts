4507cb41854de7e07c396a9a2e6f397cd4a86856
dev-jack <actionjacks22@gmail.com>
2023-09-23 20:56:21 +0200

    1

--- Angular/mapapone/appone/src/app/app.module.ts
+++ Angular/mapapone/appone/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     MessagesComponent,
     DashboardComponent,
     HeroSearchComponent,
+    HeroComponent,
   ],
   imports: [
     BrowserModule,
