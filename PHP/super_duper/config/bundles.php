a8c670e2102f2941848e5ee88995536078538ba2
Jacek Zablocki <jzablocki@netis.pl>
2025-07-25 14:49:45 +0200

    .

--- PHP/super_duper/config/bundles.php
+++ PHP/super_duper/config/bundles.php
@@ -5,4 +5,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Jacek\TestBundle\TestBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
